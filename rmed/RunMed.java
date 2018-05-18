7047f4fc2bd30883d34909b448cfce5215f901be
Jerry Ye <jerry1ye10@gmail.com>
2018-05-18 07:29:16 -0400

    Added comments

--- rmed/RunMed.java
+++ rmed/RunMed.java
@@ -24,7 +24,7 @@ public class RunMed
 	if (bigVals.size() > lilVals.size())
 	    return bigVals.peekMin();
 		
-	if (bigVals.size() < lilVals.size())
+	if (bigVals.size() < lilVals.size())//if one heap is larger than the other, return the root of the larger heap
 	    return lilVals.peekMax();
 
 	return (bigVals.peekMin() + lilVals.peekMax()) / 2;
@@ -34,11 +34,11 @@ public class RunMed
     {
 	if (lilVals.isEmpty())
 	    {
-		lilVals.add(newVal);
+		lilVals.add(newVal);//defaults to adding to MaxHeap
 		return;
 	    }
 
-	if (lilVals.peekMax() < newVal)
+	if (lilVals.peekMax() < newVal)//if insertion val is larger than largest value in lower range
 	    {
 		bigVals.add(newVal);
 	    }
@@ -47,17 +47,17 @@ public class RunMed
 		lilVals.add(newVal);
 	    }
 
-	while (lilVals.size() - bigVals.size() > 1)
+	while (lilVals.size() - bigVals.size() > 1)////if MaxHeap is larger than Minheap by more than 1, the maximum value of MaxHeap and add as root of MinHeap
 	    {
 		bigVals.add(lilVals.removeMax());
 	    }
 
-	while (bigVals.size() - lilVals.size() > 1)
+	while (bigVals.size() - lilVals.size() > 1)//if MinHeap is larger than Maxheap by more than 1, the minimum value of MinHeap and add as root of MaxHeap
 	    {
 		lilVals.add(bigVals.removeMin());
-	    }
+	    }//only one of the two while loops above will run 
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args)//test cases in driver class
     {}
 }
